48981bbde248b0dbbda79e2bf98dffae9e2f41f7
robguevarra <robneil@gmail.com>
2025-11-28 10:41:25 +0800

    facebook pixel fixed

--- components/analytics/FacebookPixel.tsx
+++ components/analytics/FacebookPixel.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "1174901340768236";
 
@@ -16,42 +16,32 @@ declare global {
 export default function FacebookPixel() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    console.log("FacebookPixel mounted, ID:", FB_PIXEL_ID);
-    setLoaded(true);
-  }, []);
-
-  useEffect(() => {
-    if (!loaded) return;
-    // Track PageView on route change
-    if (window.fbq) {
+    // Track PageView on route change and initial load
+    if (typeof window !== 'undefined' && window.fbq) {
       window.fbq("track", "PageView");
+      console.log("FacebookPixel PageView tracked");
     }
-  }, [pathname, searchParams, loaded]);
+  }, [pathname, searchParams]);
 
   return (
-    <>
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        onLoad={() => setLoaded(true)}
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${FB_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
-    </>
+    <Script
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${FB_PIXEL_ID}');
+        `,
+      }}
+    />
   );
 }
