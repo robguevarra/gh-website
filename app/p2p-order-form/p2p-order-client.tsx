ec596a5c0b53da44c4cd906c837779a33c5369f6
robguevarra <robneil@gmail.com>
2025-11-28 10:34:41 +0800

    facebook pixel fixed

--- app/p2p-order-form/p2p-order-client.tsx
+++ app/p2p-order-form/p2p-order-client.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { motion, AnimatePresence, useInView, useScroll, useTransform } from "framer-motion"
 import { BookOpen, ChevronRight, ArrowRight, Star, Check, Shield, Play, Clock, Heart } from "lucide-react"
-import Head from "next/head"
+
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
@@ -528,32 +528,7 @@ export default function PapersToProfit() {
     <>
       <PageTracker />
       {/* Facebook Meta Pixel Code for analytics and ad attribution */}
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1174901340768236');
-            fbq('track', 'PageView');`
-          }}
-        />
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1174901340768236&ev=PageView&noscript=1"
-            alt="fb pixel"
-          />
-        </noscript>
-      </Head>
+      <PageTracker />
       <div className="flex min-h-screen flex-col bg-[#f9f6f2]">
         <PublicHeader />
 
